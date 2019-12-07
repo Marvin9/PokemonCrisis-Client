ce07caf4e1d7e0f93d67a8e381efec0e0ae00394
Mayursinh <marvinduff97@gmail.com>
2019-12-07 15:18:02 +0530

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Components/Header'
+import Dashboard from './Components/Dashboard/Dashboard'
 
 function App() {
   return (
     //eslint-disable-next-line
   <React.Fragment>
     <Header />
-    
+    <Dashboard />
   </React.Fragment>
   );
 }
